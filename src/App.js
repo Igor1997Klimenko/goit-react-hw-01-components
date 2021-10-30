ee6ebb413fdb2b0d4b227f15a177fc6dc63bf247
Igor1997Klimenko <Igorek.klimuch.97@gmail.com>
2021-10-30 18:58:31 +0300

    the end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
 import Profile from './components/Profile/Profile'
 import user from '../src/components/Profile/user.json'
-
+import Statistics from './components/Statistics/Statistics'
+import statisticalData from '../src/components/Statistics/statistical-data.json'
+import FriendList from './components/FriendList/FriendList'
+import friends from '../src/components/FriendList/friends.json'
+import TransactionHistory from '../src/components/TransactionHistory/TransactionHistory'
+import transactions from '../src/components/TransactionHistory/transactions.json'
 const App = () => (
 <>
     <Profile
@@ -10,7 +15,10 @@ const App = () => (
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-    />
+        />,
+    <Statistics title="Upload stats" stats={statisticalData} />,
+    <FriendList friends={friends} />,
+    <TransactionHistory items={transactions} />,
 </>
     
 );
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => (
+    <section className={style.statistics}>
+        <h2 className={style.title}>{title}</h2>
+        <ul className={style.statlist}>
+            {stats.map(stat => (
+                <li className={style.item} key={stat.id}>
+                    <span className={style.label}>{stat.label}</span>
+                    <span className={style.percentage}>{stat.percentage}%</span>
+                </li>
+            ))}
+        </ul>
+    </section>
+);
+
+Statistics.propTypes = {
+    title: PropTypes.string.isRequired,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        }),
+    ),
+};
+export default Statistics;
